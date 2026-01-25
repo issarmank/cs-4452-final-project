05304d66e67e98379b60231520ef3b448c3dac3a
Jonathan Peters <jpete29@uwo.ca>
2026-01-24 20:26:23 -0500

    Base commit, just making the base files

---
+++ model.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+class DynamicNN(nn.Module):
+    def __init__(self, input_size, hidden_sizes, num_layers, output_size):
+        super().__init__()
+        #Make the rest of this later
+    
+    def forward(self, x):
+        # Will work on this later
+        pass
