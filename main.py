7a52bd3d7bb9094dea1136e54bb904b2afe66cb3
Abdisalam Halane <halaneabdii@hotmail.com>
2026-03-07 21:49:59 -0500

    test run

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ from torch.utils.data import DataLoader
 from model import DynamicNN
 from train_test_loop import train_model, test_model
 
-#Not seting up device rn, can add later if we want
-
 #Loading the MNIST dataset
 train_data = datasets.MNIST(root='data', train=True, download=True, transform=ToTensor())
 test_data = datasets.MNIST(root='data', train=False, download=True, transform=ToTensor())
@@ -24,16 +22,11 @@ NUM_CLASSES = len(train_data.classes) # 10 output classes for MNIST
 train_dataloader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True)
 test_dataloader = DataLoader(test_data, batch_size=BATCH_SIZE, shuffle=False)
 
-"""
-This is an example of how to run the loops with a dynamic nn. The hidden_layers list can be changed to make any architecture
-The train and test model functions print the loss and accuracy, this got 96% accuracy with 5 epochs
--------------------------------------------------------------------------------------
-hidden_layers = [INPUT_SIZE, 64, 32, 16, NUM_CLASSES]
+hidden_layers = [784, 64, 32, 16, 10]
 model = DynamicNN(hidden_layers)
 loss_fn = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
 model = train_model(model, train_dataloader, test_dataloader, loss_fn, optimizer, 5)
 test_model(model, test_dataloader, loss_fn)
-"""
 
